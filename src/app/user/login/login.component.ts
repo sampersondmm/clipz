47f7c9ac8ca7db4d3db5634308f078db95c7f33a
Sam Person <sampersondmm@gmail.com>
2024-02-09 17:15:53 -0500

    Finished home page and user form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: '',
+  };
 
+  login() {
+    console.log(this.credentials);
+  }
 }
