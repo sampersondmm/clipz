cb794e4294a8f138e0a848f6a9cbbb6054e2f7f8
Sam Person <sampersondmm@gmail.com>
2024-02-09 23:19:51 -0500

    setup firestore, added user registration

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyDyopu-wXbPVEEMIEAqXOvGGXIGuSyKero',
+    authDomain: 'clipz-585e2.firebaseapp.com',
+    projectId: 'clipz-585e2',
+    storageBucket: 'clipz-585e2.appspot.com',
+    messagingSenderId: '165447797790',
+    appId: '1:165447797790:web:20218b2232f9c923ab320f',
+  },
+};
